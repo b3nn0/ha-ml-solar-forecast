2e96cb03122b860f6365b99af01cf8c6e3e03ae1
Adrian Batzill <dev@batzill.com>
2026-03-09 12:07:32 +0100

    fix: update polling

--- custom_components/ml_solar_forecast/__init__.py
+++ custom_components/ml_solar_forecast/__init__.py
@@ -1,7 +1,7 @@
 """The HA-ML Solar Forecast integration."""
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_NAME
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
@@ -17,12 +17,17 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up HA-ML Solar Forecast from a config entry."""
     coordinator = MLSolarForecastCoordinator(hass, entry)
-    await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-    # await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS) # ??
+
+    # await hass.config_entries.async_forward_entry_setups(entry, [Platform.SENSOR])
+
+    # coordinator is only updated if there are listeners. We have no entities for now - create dummy listener.
+    entry.async_on_unload(coordinator.async_add_listener(lambda: None))
 
     entry.async_on_unload(entry.add_update_listener(async_update_options))
+
+    await coordinator.async_config_entry_first_refresh()
 
     return True
 
